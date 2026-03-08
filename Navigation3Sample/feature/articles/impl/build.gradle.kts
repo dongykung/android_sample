a5412976c438c67c7ae2b5fbadc7255895e029aa
kim dong kyung <kdkd422@naver.com>
2026-03-08 21:30:30 +0900

    build: impl 모듈 nav3 runtime 의존성 추가

--- Navigation3Sample/feature/articles/impl/build.gradle.kts
+++ Navigation3Sample/feature/articles/impl/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
