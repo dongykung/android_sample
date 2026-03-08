1ef41d505612797fcf5219fbf8f6ded70c706eaa
kim dong kyung <kdkd422@naver.com>
2026-03-08 20:45:29 +0900

    build: feature:api 모듈 serialization plugin 추가

--- Navigation3Sample/feature/articles/api/build.gradle.kts
+++ Navigation3Sample/feature/articles/api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -34,6 +35,7 @@ android {
 
 dependencies {
     api(project(":core:navigator"))
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
